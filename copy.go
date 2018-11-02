0e4394df445d9553ff0018a12172e95787ef9c75
Matt Poepping <mattpoepping@Matts-MacBook-Pro-2.local>
2018-11-02 14:14:46 -0600

    cleaning up code

--- copy.go
+++ copy.go
@@ -21,15 +21,7 @@ func startService() {
   box := packr.NewBox("./templates")
 
   http.Handle("/", http.FileServer(box))
-
-  /*
-  http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(rw, "Hello world!")
-  })
-  */
-    http.HandleFunc("/upload/", ReceiveFile)
-
-  //http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+  http.HandleFunc("/upload/", ReceiveFile)
 
   log.Println("starting http service...")
   if err := http.ListenAndServe(":80", nil); err != nil {
@@ -61,21 +53,10 @@ func ReceiveFile(w http.ResponseWriter, r *http.Request) {
     name := strings.Split(header.Filename, ".")
     fmt.Printf("File name %s\n", name[0])
     fmt.Printf("Full name %s\n", header.Filename)
-    
-    // Copy the file data to my buffer
     io.Copy(&Buf, file)
-    // do something with the contents...
-    // I normally have a struct defined and unmarshal into a struct, but this will
-    // work as an example
     contents := Buf.String()
-    //fmt.Println(contents)
     ioutil.WriteFile(header.Filename, []byte(contents) , 0644)
-    // I reset the buffer in case I want to use it again
-    // reduces memory allocations in more intense projects
     Buf.Reset()
-    // do something else
-    // etc write header
-    fmt.Fprintln(w, "Hello world!")
     return
 }
 
@@ -84,14 +65,10 @@ func main() {
   var dns_string string
 
   // Start out http service
-  go startService()
-  //log.SetFlags(0)
-  //log.SetOutput(ioutil.Discard)
- 
+  go startService() 
   ip = GetOutboundIP()
   dns_string = fmt.Sprintf("copy.local 60 IN A %s", ip)
   fmt.Println(dns_string)
-  //mdns.Publish("copy.local 60 IN A 192.168.0.118")
   mdns.Publish(dns_string)
 
   // Sleep forever
